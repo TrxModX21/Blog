8886ca5b2d29de7ccccf5ab7bcecb0ac07433fb9
TrxModX21 <trxcode21@gmail.com>
2024-01-20 18:30:38 +0700

    Modelling the data

---
+++ models/post/Post.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+// SCHEMA
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "react",
+        "nodejs",
+        "html",
+        "css",
+        "expressjs",
+        "tailwindcss",
+        "javascript",
+        "other",
+      ],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Middlewares
 // Routes
 // Error handler middlewares
-// Listen server
 
+// Listen server
 const PORT = process.env.PORT || 2121;
 app.listen(PORT, console.log(`server is running on PORT: ${PORT}`));
